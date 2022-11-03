a98610ba91c67ad9536d4f963c161f52456eb2c5
Saif-Alharbi <saifalharbi1421@gmail.com>
2022-11-03 19:40:43 +0300

    formatting

--- script.js
+++ script.js
@@ -1,45 +1,46 @@
-submitBtn = document.getElementById('submitButton') 
+submitBtn = document.getElementById("submitButton");
 function validateInput(elementId, min, max) {
-    const element = document.getElementById(`${elementId}`)
-    const elementLabel = document.getElementById(`${elementId}Error`)
+  const element = document.getElementById(`${elementId}`);
+  const elementLabel = document.getElementById(`${elementId}Error`);
 
-    element.addEventListener('keyup', (handler) => {
-        const elementValue = element.value
+  element.addEventListener("keyup", (handler) => {
+    const elementValue = element.value;
 
-        elementValue.length < min || elementValue.length > max
-            ? elementLabel.classList.add('error-message-visible')
-            : elementLabel.classList.remove('error-message-visible')
+    elementValue.length < min || elementValue.length > max
+      ? elementLabel.classList.add("error-message-visible")
+      : elementLabel.classList.remove("error-message-visible");
 
-        elementLabel.classList.contains('error-message-visible')
-            ? submitBtn.disabled = true
-            : submitBtn.disabled = false
-    })
+    elementLabel.classList.contains("error-message-visible")
+      ? (submitBtn.disabled = true)
+      : (submitBtn.disabled = false);
+  });
 }
 
-const checkLogin = document.getElementById('loginEmail')
-if(checkLogin){
-    validateInput('loginEmail', 8, 24)
-    validateInput('loginPassword', 8, 16)
+const checkLogin = document.getElementById("loginEmail");
+if (checkLogin) {
+  validateInput("loginEmail", 8, 24);
+  validateInput("loginPassword", 8, 16);
 }
-const checkReg = document.getElementById('fullName')
-if(checkReg){
-    validateInput('fullName', 5, 24)
-    validateInput('userName', 5, 16)
-    validateInput('email', 8, 24)
-    validateInput('password', 8, 16)
-    const checkPassword = document.getElementById('confirmedPassword')
-    checkPassword.addEventListener('keyup', (handler) => {
-        const checkPasswordValue = checkPassword.value
-        const passwordValue = document.getElementById('password').value
-        const confirmedPasswordLabel = document.getElementById('confirmedPasswordError')
+const checkReg = document.getElementById("fullName");
+if (checkReg) {
+  validateInput("fullName", 5, 24);
+  validateInput("userName", 5, 16);
+  validateInput("email", 8, 24);
+  validateInput("password", 8, 16);
+  const checkPassword = document.getElementById("confirmedPassword");
+  checkPassword.addEventListener("keyup", (handler) => {
+    const checkPasswordValue = checkPassword.value;
+    const passwordValue = document.getElementById("password").value;
+    const confirmedPasswordLabel = document.getElementById(
+      "confirmedPasswordError"
+    );
 
-        !(checkPasswordValue === passwordValue)
-            ? confirmedPasswordLabel.classList.add('error-message-visible')
-            : confirmedPasswordLabel.classList.remove('error-message-visible')
+    !(checkPasswordValue === passwordValue)
+      ? confirmedPasswordLabel.classList.add("error-message-visible")
+      : confirmedPasswordLabel.classList.remove("error-message-visible");
 
-        confirmedPasswordLabel.classList.contains('error-message-visible')
-            ? submitBtn.disabled = true
-            : submitBtn.disabled = false
-    })
+    confirmedPasswordLabel.classList.contains("error-message-visible")
+      ? (submitBtn.disabled = true)
+      : (submitBtn.disabled = false);
+  });
 }
-
